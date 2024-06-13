65af93c65d8b0ed679c510259b222e0659a89d60
Wilton-Monteiro <145207587+Wilton-Monteiro@users.noreply.github.com>
2024-06-12 23:11:47 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const sql = require('mssql');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Configuração do banco de dados
 const config = {
@@ -13,13 +13,14 @@ const config = {
     server: 'morerao-server.database.windows.net',
     database: 'heroivilao',
     options: {
-        encrypt: true // Dependendo da configuração do seu servidor SQL Server
+        encrypt: true,
+        enableArithAbort: true
     }
 };
 
-// Configurar CORS para permitir requisições de 'https://tarefadatabase.vercel.app'
+// Configurar CORS para permitir requisições de 'https://pisafecar.vercel.app'
 app.use(cors({
-    origin: 'https://tarefadatabase.vercel.app'
+    origin: 'https://pisafecar.vercel.app'
 }));
 
 app.use(express.json());
@@ -34,15 +35,33 @@ app.post('/atualizarVida', async (req, res) => {
     try {
         await sql.connect(config);
         const request = new sql.Request();
+
+        // Atualizar a vida do herói
         await request.query(`
             MERGE INTO personagem AS target
-            USING (VALUES ('heroi', ${vidaHeroi}), ('vilao', ${vidaVilao})) AS source (nome, vida)
+            USING (VALUES ('heroi', @vidaHeroi)) AS source (nome, vida)
             ON target.nome = source.nome
             WHEN MATCHED THEN
                 UPDATE SET vida = source.vida
             WHEN NOT MATCHED THEN
                 INSERT (nome, vida) VALUES (source.nome, source.vida);
-        `);
+        `, {
+            vidaHeroi
+        });
+
+        // Atualizar a vida do vilão
+        await request.query(`
+            MERGE INTO personagem AS target
+            USING (VALUES ('vilao', @vidaVilao)) AS source (nome, vida)
+            ON target.nome = source.nome
+            WHEN MATCHED THEN
+                UPDATE SET vida = source.vida
+            WHEN NOT MATCHED THEN
+                INSERT (nome, vida) VALUES (source.nome, source.vida);
+        `, {
+            vidaVilao
+        });
+
         res.status(200).send('Vida do herói e do vilão atualizada com sucesso.');
     } catch (err) {
         console.error(err);
@@ -80,37 +99,48 @@ app.post('/inserirUsuario', async (req, res) => {
         const request = new sql.Request();
         await request.query(`
             MERGE INTO usuario AS target
-            USING (VALUES ('${usuario}', '${senha}')) AS source (usuario, senha)
+            USING (VALUES (@usuario, @senha)) AS source (usuario, senha)
             ON target.usuario = source.usuario
             WHEN MATCHED THEN
                 UPDATE SET senha = source.senha
             WHEN NOT MATCHED THEN
                 INSERT (usuario, senha) VALUES (source.usuario, source.senha);
-        `);
-        res.status(200).send('Usuario cadastrado com sucesso.');
+        `, {
+            usuario,
+            senha
+        });
+        res.status(200).send('Usuário cadastrado com sucesso.');
     } catch (err) {
         console.error(err);
-        res.status(500).send('Erro ao inserir usuario.');
+        res.status(500).send('Erro ao inserir usuário.');
     }
 });
 
 // Rota para validar um usuário
 app.get('/validarUsuario', async (req, res) => {
+    const { usuario, senha } = req.query;
+
     try {
-        const { usuario, senha } = req.query;
         await sql.connect(config);
         const request = new sql.Request();
 
-        // Consulta para obter os dados do usuário
-        const userResult = await request.query(`SELECT * FROM usuario WHERE usuario = '${usuario}' AND senha = '${senha}'`);
-        const user = userResult.recordset[0];
+        // Consulta para validar o usuário
+        const result = await request.query(`
+            SELECT * FROM usuario WHERE usuario = @usuario AND senha = @senha
+        `, {
+            usuario,
+            senha
+        });
+
+        const user = result.recordset[0];
         if (!user) {
-            return res.status(404).json({ error: 'Usuario não cadastrado' });
+            return res.status(404).json({ error: 'Usuário não cadastrado' });
         }
 
-        res.json({ usuario, senha });
-    } catch (error) {
-        res.status(500).json({ error: 'Erro ao buscar dados do usuario.' });
+        res.status(200).json({ message: 'Login bem sucedido.' });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Erro ao validar usuário.' });
     }
 });
 
@@ -119,12 +149,12 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'front-game/login.html'));
 });
 
-app.get('/game', (req, res) => {
-    res.sendFile(path.join(__dirname, 'front-game/game.html'));
-});
-
 app.get('/dashboard', (req, res) => {
     res.sendFile(path.join(__dirname, 'front-game/dashboard.html'));
+});
+
+app.get('/game', (req, res) => {
+    res.sendFile(path.join(__dirname, 'front-game/game.html'));
 });
 
 // Iniciar o servidor
